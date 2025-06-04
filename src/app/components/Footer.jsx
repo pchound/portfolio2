ade501b692e265c7e3065f2c5b726c61f792ae56
pchound <chaonerdguy@gmail.com>
2025-06-04 17:25:14 -0600

    resume-and-footer-update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <footer className="footer">
-            <div className='container pt-0 pr-10 pb-10 pl-10 flex justify-between'>
+            <div className='container pt-0 pr-10 pb-10 pl-10 flex flex-col items-center text-center sm:flex-row sm:justify-between sm:text-right'>
                 <span></span>
-                <p className='text-slate-600 text-right'>&copy; 2025 Joseph Web Designs<br></br>
-                    All Rights Reserved</p>
+                <p className='text-slate-600'>&copy; {currentYear} Joseph Web Designs<br />All Rights Reserved</p>
             </div>
-
         </footer>
-
     )
 }
+
 export default Footer;
