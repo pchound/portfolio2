187aea3a87c9de01f6c677f503c4f42c2580270a
pchound <chaonerdguy@gmail.com>
2025-06-04 17:54:48 -0600

    mobile-image-adjust

--- src/app/components/animated-section1/AnimatedImage.jsx
+++ src/app/components/animated-section1/AnimatedImage.jsx
@@ -14,7 +14,7 @@ const AnimatedImage = () => {
             width={300}
             height={300}
             className={`
-        rounded-full relative top-6 left-6 transition-all duration-1000 ease-out transform
+        rounded-full relative top-6 sm:left-4 transition-all duration-1000 ease-out transform
         drop-shadow-[0_0_25px_rgba(0,0,0,0.5)]
         ${isInView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}
       `}
