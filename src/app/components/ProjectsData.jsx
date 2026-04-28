f9dbeecc7cc4b3443eed02e34de4621133eba1e3
pchound <chaonerdguy@gmail.com>
2026-04-28 12:09:47 -0600

    added-zing

--- src/app/components/ProjectsData.jsx
+++ src/app/components/ProjectsData.jsx
@@ -8,15 +8,7 @@ const projectsData = [
         gitUrl: "https://github.com/pchound/QuickCart",
         previewUrl: "https://quick-cart-4unup3dzy-josephs-projects-f923fa52.vercel.app/",
     },*/
-    {
-        id: 2,
-        title: "Endless Road Auto Sales",
-        description: "Car sales website (PHP/SQL)",
-        image: "/images/projects/2.png",
-        tag: ["All", "Web"],
-        gitUrl: "https://github.com/pchound/erautosales",
-        previewUrl: "http://www.erautosales.com",
-    },
+,
     {
         id: 3,
         title: "Ischys Wellness",
@@ -25,8 +17,25 @@ const projectsData = [
         tag: ["All", "Web"],
         gitUrl: "https://github.com/pchound/ischys",
         previewUrl: "https://ischys.vercel.app/",
+    },
+    {
+        id: 4,
+        title: "Zing Pest",
+        description: "Zing Pest Control (Next.js)",
+        image: "/images/projects/5.jpg",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/pchound/zing",
+        previewUrl: "https://www.zingpest.com/",
+    },
+        {
+        id: 5,
+        title: "Endless Road Auto Sales",
+        description: "Car sales website (PHP/SQL)",
+        image: "/images/projects/2.png",
+        tag: ["All", "Web"],
+        gitUrl: "https://github.com/pchound/erautosales",
+        previewUrl: "http://www.erautosales.com",
     }
-
 
 ];
 
